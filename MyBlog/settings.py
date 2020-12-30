979581f43f0c93af7334564314026dfcdcfb6a5e
ameerarx <ameerarx@outlook.com>
2020-12-31 00:14:48 +0500

    2

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'article.apps.ArticleConfig',
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- article/models.py
+++ article/models.py
@@ -10,12 +10,16 @@ class Article(models.Model):
         return self.article_title
     def was_published_recently(self):
         return self.pub_date >= (timezone.now() - datetime.timedelta(days=7)) 
-
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete = models.CASCADE)
     author_name = models.CharField("имя автора", max_length=50)
     comment_text = models.CharField("текст коментария", max_length = 200)
     def __str__(self):
         return self.author_name
-
+    class Meta:
+        verbose_name = "Коментарий"
+        verbose_name_plural = "Коментарии"
 
