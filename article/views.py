3f3df72949ba7a3736b31fb64806dedc22d7fab8
ameerarx <ameerarx@outlook.com>
2021-01-05 06:21:20 +0500

    3

--- article/views.py
+++ article/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404,HttpResponseRedirect
+from .models import Article,Comment
 def index(request):
-    return HttpResponse("Hello")
-
+    latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    return render(request,'articles/list.html',{'latest_articles_list':latest_articles_list})
+def details(request, article_id):
+    try :
+        a = Article.objects.get(pk = article_id)
+        latest_articles_list = Article.objects.order_by('-pub_date')[:5]
+    except:
+        raise Http404("Not Found")
+    return render(request,'articles/detail.html',{'articles':a,'latest_articles_list':latest_articles_list})    
